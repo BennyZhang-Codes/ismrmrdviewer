1be55abebdfe16c1b1d24f331c20d28ff44ec2ba
Jinyuan Zhang <823573476@qq.com>
2026-03-10 18:01:27 +0800

    update run.py

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import sys
+import os
 from PySide6.QtWidgets import QApplication
 from PySide6.QtGui import QIcon
 
@@ -7,10 +8,22 @@ from icon import icon_rc
 
 def main():
     app = QApplication(sys.argv)
+    
+    app_icon = QIcon(":icon.ico")
+    app.setWindowIcon(app_icon)
+
     mainWindow = MainWindow()
-    mainWindow.setWindowIcon(QIcon(":icon.ico"))
+    mainWindow.setWindowIcon(app_icon)
     mainWindow.setWindowTitle('ISMRMRD Viewer - Jinyuan Zhang')
     mainWindow.resize(1200, 800)
+    
+    if len(sys.argv) > 1:
+        target_file = sys.argv[1]
+        if os.path.exists(target_file) and os.path.isfile(target_file):
+            folder_path = os.path.dirname(target_file)
+            mainWindow.set_workspace_path(folder_path)
+            mainWindow.load_file_to_tab(target_file)
+
     mainWindow.show()
     sys.exit(app.exec())
 
